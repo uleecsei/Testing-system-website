999b6d5dcd90fb44039db4a6545dbb296e117ad3
Alexei <uleecsei@gmail.com>
2020-01-27 19:25:31 +0200

    IE supporting features

--- src/app/contentPage/main/main_content.js
+++ src/app/contentPage/main/main_content.js
@@ -1,5 +1,10 @@
 import firebase from 'firebase';
 
+// missing forEach on NodeList for IE11
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
 let currentPage = null;
 let defaultPages = {
     html: [],
